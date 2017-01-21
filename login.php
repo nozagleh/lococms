5a34b0698b8e02d6dc132b3a4854c82f69ba6b20
Arnar Freyr Helgason <nozagleh@gmail.com>
2017-01-21 17:47:13 +0100

    create login and logout files

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+  session_start();
+  require('query.php');
+
+  if (isset($_SESSION["user"]))
+    header('location:admin.php');
+
+  if (isset($_POST["username"]))
+    checkLogin($_POST["username"], $_POST["password"]);
+
+  function checkLogin($user, $pass){
+    $is_valid = Query::verifyLogin($user,$pass);
+
+    if ($is_valid) {
+      $_SESSION["user"] = $user;
+      header('location:admin.php');
+    }
+  }
+
+  ?>
+  <!DOCTYPE html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="core/styles/loco-base.css">
+    </head>
+    <body>
+      <div class="wrapper">
+        <div class="loginform">
+          <form action="" method="POST" id="form">
+            <label>Username:</label><input type="text" name="username" /><br/>
+            <label>Password:</label><input type="password" name="password" /><br />
+          </form>
+          <button type="submit" form="form" value="Submit">Login</button>
+          <a href="index.php">Go back to site</a>
+        </div>
+      </div>
+    </body>
+  </html>
