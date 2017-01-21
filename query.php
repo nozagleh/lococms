b6fa7d6ae9f362377a9475b18a0386975132da40
Arnar Freyr Helgason <nozagleh@gmail.com>
2017-01-22 00:44:48 +0100

    add function to get certain element and to get post content

--- query.php
+++ query.php
@@ -1,29 +1,76 @@
 <?php
 
-  class Query {
+  class Loco_Query {
 
-    function __construct() {
+    //empty constructor
+    function __construct() {}
 
-
-    }
-
+    /*
+    *Function for verifying users
+    *@return bool true/false
+    */
     function verifyLogin($username, $password) {
       try {
+        //initiate a new connection
         $pdo = new PDO("mysql:dbname=loco-db;host=localhost",'root','');
-        $query = $pdo->prepare("SELECT * FROM `loco-users` WHERE `username` LIKE :user AND `passwd` LIKE :pass;");
+
+        //prepare the query to check user data, bind parameters
+        $query = $pdo->prepare("SELECT `username`,`passwd` FROM `loco-users` WHERE `username` LIKE :user AND `passwd` LIKE :pass;");
         $query->bindParam(':user', $username);
         $query->bindParam(':pass', $password);
 
+        //execute the query and fetch the results
         $query->execute();
         $results = $query->fetchAll();
 
+        //if results are not empty, compare with user provided information
         if (!empty($results)) {
-          return true;
+          foreach ($results as $key => $value) {
+            //if username and password match, then return true
+            if (($username == $value["username"] && $password == $value["passwd"])) {
+              return true;
+            }
+          }
         }
       } catch (PDOException $e) {
+        //return error message
         return $e->getMessage();
       }
+      //return false if nothing is found
       return false;
+    }
+
+    function getPostsContent($pageid) {
+  		$pdo = new PDO('mysql:host=localhost;dbname=loco-db','root','');
+
+  		try{
+  			$query = $pdo->prepare("SELECT * FROM `loco-post` WHERE `fk_page` = :pageid");
+  			$query->bindParam(":pageid", $pageid);
+  			$query->execute();
+
+  			$results = $query->fetchAll();
+
+        return $results;
+  		}catch(PDOException $e) {
+  			return($e->getMessage());
+  		}
+
+  	}
+
+    function getCertainElement($element) {
+      $pdo = new PDO('mysql:host=localhost;dbname=loco-db','root','');
+
+  		try{
+        $query = $pdo->prepare("SELECT * FROM `loco-meta` WHERE `type` = :element");
+        $query->bindParam(":element", $element);
+  			$query->execute();
+
+  			$results = $query->fetchAll();
+
+        return $results;
+  		}catch(PDOException $e) {
+  			return($e->getMessage());
+  		}
     }
 
   }
