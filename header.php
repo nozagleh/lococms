7683b93d9257db764e56201c244b70c8b2bcbfeb
Arnar Freyr Helgason <nozagleh@gmail.com>
2017-01-21 17:46:47 +0100

    init header and footer files

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+  <div class="logo">
+    <h1>Logo</h1>
+  </div>
+  <nav>
+    <li><a href="">Item</a></li>
+    <li><a href="">Item</a></li>
+    <li><a href="">Item</a></li>
+    <li><a href="">Item</a></li>
+  </nav>
+</header>
