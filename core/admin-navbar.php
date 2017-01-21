5e37fe01db032a49a402c2da61b0c3f9637d1b53
Arnar Freyr Helgason <nozagleh@gmail.com>
2017-01-22 00:40:50 +0100

    create global navbar

---
+++ core/admin-navbar.php
@@ -0,0 +1,13 @@
+<?php
+
+  function getAdminNav() {
+    ?>
+    <nav>
+      <li><a href="editor.php?element=menu">Menu</a></li>
+      <li><a href="editor.php?element=footer">Footer</a></li>
+      <li><a href="logout.php">Logout</a></li>
+    </nav>
+    <?php
+  }
+
+ ?>
